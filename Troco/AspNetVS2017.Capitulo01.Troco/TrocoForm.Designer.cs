e1411f8ddb7caca4ebf230dd5096c705714685e9
PedroPini <augustopini@gmail.com>
2018-11-21 20:57:30 -0200

    Adicionando novo projeto Troco

---
+++ Troco/AspNetVS2017.Capitulo01.Troco/TrocoForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace AspNetVS2017.Capitulo01.Troco
+{
+    partial class TrocoForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.calcularButton = new System.Windows.Forms.Button();
+            this.valorCompraTextBox = new System.Windows.Forms.TextBox();
+            this.valorPagoTextBox = new System.Windows.Forms.TextBox();
+            this.trocoTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.trocoTextBox);
+            this.groupBox1.Controls.Add(this.valorPagoTextBox);
+            this.groupBox1.Controls.Add(this.valorCompraTextBox);
+            this.groupBox1.Controls.Add(this.calcularButton);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(267, 426);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Valores";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor da &compra:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Valor &pago:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "&Troco:";
+            // 
+            // calcularButton
+            // 
+            this.calcularButton.Location = new System.Drawing.Point(9, 165);
+            this.calcularButton.Name = "calcularButton";
+            this.calcularButton.Size = new System.Drawing.Size(252, 23);
+            this.calcularButton.TabIndex = 4;
+            this.calcularButton.Text = "C&alcular";
+            this.calcularButton.UseVisualStyleBackColor = true;
+            // 
+            // valorCompraTextBox
+            // 
+            this.valorCompraTextBox.Location = new System.Drawing.Point(99, 27);
+            this.valorCompraTextBox.Name = "valorCompraTextBox";
+            this.valorCompraTextBox.Size = new System.Drawing.Size(162, 20);
+            this.valorCompraTextBox.TabIndex = 1;
+            // 
+            // valorPagoTextBox
+            // 
+            this.valorPagoTextBox.Location = new System.Drawing.Point(99, 72);
+            this.valorPagoTextBox.Name = "valorPagoTextBox";
+            this.valorPagoTextBox.Size = new System.Drawing.Size(162, 20);
+            this.valorPagoTextBox.TabIndex = 2;
+            // 
+            // trocoTextBox
+            // 
+            this.trocoTextBox.Location = new System.Drawing.Point(99, 117);
+            this.trocoTextBox.Name = "trocoTextBox";
+            this.trocoTextBox.Size = new System.Drawing.Size(162, 20);
+            this.trocoTextBox.TabIndex = 3;
+            // 
+            // TrocoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "TrocoForm";
+            this.Text = "Troco";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button calcularButton;
+        private System.Windows.Forms.TextBox trocoTextBox;
+        private System.Windows.Forms.TextBox valorPagoTextBox;
+        private System.Windows.Forms.TextBox valorCompraTextBox;
+    }
+}
+
