e38f91bdd5a685c7afea901430487317f3f897cb
PedroPini <augustopini@gmail.com>
2018-11-21 22:38:54 -0200

    Adicionando novo projeto de frete

---
+++ Frete/AspNetVS2017.Capitulo01.Frete/FreteForm.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace AspNetVS2017.Capitulo01.Frete
+{
+    partial class FreteForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.calcularButton = new System.Windows.Forms.Button();
+            this.limparButton = new System.Windows.Forms.Button();
+            this.clienteTextBox = new System.Windows.Forms.TextBox();
+            this.valorTextBox = new System.Windows.Forms.TextBox();
+            this.freteTextBox = new System.Windows.Forms.TextBox();
+            this.totalTextBox = new System.Windows.Forms.TextBox();
+            this.ufComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cliente";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "UF";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Valor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Frete";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Total:";
+            // 
+            // calcularButton
+            // 
+            this.calcularButton.Location = new System.Drawing.Point(264, 285);
+            this.calcularButton.Name = "calcularButton";
+            this.calcularButton.Size = new System.Drawing.Size(75, 23);
+            this.calcularButton.TabIndex = 6;
+            this.calcularButton.Text = "Calcular";
+            this.calcularButton.UseVisualStyleBackColor = true;
+            // 
+            // limparButton
+            // 
+            this.limparButton.Location = new System.Drawing.Point(264, 314);
+            this.limparButton.Name = "limparButton";
+            this.limparButton.Size = new System.Drawing.Size(75, 23);
+            this.limparButton.TabIndex = 7;
+            this.limparButton.Text = "Limpar";
+            this.limparButton.UseVisualStyleBackColor = true;
+            // 
+            // clienteTextBox
+            // 
+            this.clienteTextBox.Location = new System.Drawing.Point(57, 4);
+            this.clienteTextBox.Name = "clienteTextBox";
+            this.clienteTextBox.Size = new System.Drawing.Size(100, 20);
+            this.clienteTextBox.TabIndex = 1;
+            // 
+            // valorTextBox
+            // 
+            this.valorTextBox.Location = new System.Drawing.Point(57, 79);
+            this.valorTextBox.Name = "valorTextBox";
+            this.valorTextBox.Size = new System.Drawing.Size(100, 20);
+            this.valorTextBox.TabIndex = 3;
+            // 
+            // freteTextBox
+            // 
+            this.freteTextBox.Location = new System.Drawing.Point(57, 116);
+            this.freteTextBox.Name = "freteTextBox";
+            this.freteTextBox.ReadOnly = true;
+            this.freteTextBox.Size = new System.Drawing.Size(100, 20);
+            this.freteTextBox.TabIndex = 4;
+            // 
+            // totalTextBox
+            // 
+            this.totalTextBox.Location = new System.Drawing.Point(57, 153);
+            this.totalTextBox.Name = "totalTextBox";
+            this.totalTextBox.ReadOnly = true;
+            this.totalTextBox.Size = new System.Drawing.Size(100, 20);
+            this.totalTextBox.TabIndex = 5;
+            // 
+            // ufComboBox
+            // 
+            this.ufComboBox.FormattingEnabled = true;
+            this.ufComboBox.Items.AddRange(new object[] {
+            "SP",
+            "MG",
+            "SC",
+            "RJ",
+            "AM",
+            "Outros"});
+            this.ufComboBox.Location = new System.Drawing.Point(58, 41);
+            this.ufComboBox.Name = "ufComboBox";
+            this.ufComboBox.Size = new System.Drawing.Size(121, 21);
+            this.ufComboBox.TabIndex = 2;
+            // 
+            // FreteForm
+            // 
+            this.AcceptButton = this.calcularButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.limparButton;
+            this.ClientSize = new System.Drawing.Size(368, 373);
+            this.Controls.Add(this.ufComboBox);
+            this.Controls.Add(this.totalTextBox);
+            this.Controls.Add(this.freteTextBox);
+            this.Controls.Add(this.valorTextBox);
+            this.Controls.Add(this.clienteTextBox);
+            this.Controls.Add(this.limparButton);
+            this.Controls.Add(this.calcularButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FreteForm";
+            this.Text = "Frete";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button calcularButton;
+        private System.Windows.Forms.Button limparButton;
+        private System.Windows.Forms.TextBox clienteTextBox;
+        private System.Windows.Forms.TextBox valorTextBox;
+        private System.Windows.Forms.TextBox freteTextBox;
+        private System.Windows.Forms.TextBox totalTextBox;
+        private System.Windows.Forms.ComboBox ufComboBox;
+    }
+}
+
