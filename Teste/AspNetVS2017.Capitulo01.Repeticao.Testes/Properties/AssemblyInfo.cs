b238d2949865625012c39d2e1dcbc444ae34d805
PedroPini <augustopini@gmail.com>
2018-11-22 22:40:20 -0200

    adicionando  projeto Teste

---
+++ Teste/AspNetVS2017.Capitulo01.Repeticao.Testes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AspNetVS2017.Capitulo01.Repeticao.Testes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AspNetVS2017.Capitulo01.Repeticao.Testes")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5821e25c-8133-4a0a-8201-604c1e415920")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Teste/AspNetVS2017.Capitulo01.Repeticao.Testes/RepeticaoTeste.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AspNetVS2017.Capitulo01.Repeticao.Testes
+{
+    [TestClass]
+    public class RepeticaoTeste
+    {
+        [TestMethod]
+        public void ForAninhadoTeste()
+        {
+            for (int i =0; i <= 10; i++)
+            {
+                for (int j = 0; j <= 10; j++)
+                {
+                    Console.WriteLine($"{i} X {j} é igual a {i * j}");
+                }
+                Console.WriteLine(new string('-', 50));
+            }
+        }
+
+        [TestMethod]
+        public void EstruturaForTeste()
+        {
+            var i = 1;
+
+            for (Console.WriteLine("Iniciou"); i <= 3; Console.WriteLine(i))
+            {
+                i++;
+            }
+
+            /*
+              *for (1.Inicialização; 2.Condição de execução; 4.Pós-execução)
+                {
+                    3.Execução;
+                }
+             * */
+
+
+        }
+
+        [TestMethod]
+        public void ForApenasCondicaoTeste()
+        {
+            var i = 1;
+
+            for (; i <= 3;)
+            {
+                Console.WriteLine("i: " + i++);
+            }
+        }
+    }
+}
