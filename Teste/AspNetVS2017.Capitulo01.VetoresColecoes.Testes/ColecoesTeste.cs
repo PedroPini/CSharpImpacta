4255377b5008ea6fd037af3f1904ea789b1a081e
PedroPini <augustopini@gmail.com>
2018-11-23 21:56:08 -0200

    Criando novo projeto trabalhando com List

---
+++ Teste/AspNetVS2017.Capitulo01.VetoresColecoes.Testes/ColecoesTeste.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetVS2017.Capitulo01.VetoresColecoes.Testes
+{
+    [TestClass]
+    public class ColecoesTeste
+    {
+        [TestMethod]
+        public void ListTest()
+        {
+            var inteiros = new List<int>(50) { 2, 1, 2, 6, 125};
+            inteiros.Add(22);
+
+            var maisInteiros = new List<int> { 21, 72, 14 };
+
+            inteiros.AddRange(maisInteiros);
+
+            inteiros.Insert(0, 29);
+
+            inteiros.Remove(21);//elemento
+            inteiros.RemoveAt(5);//index
+
+            inteiros.Sort();
+
+
+            //var primeiro = inteiros.First();
+            var primeiro = inteiros.FirstOrDefault();
+            var ultimo = inteiros[inteiros.Count -1];
+            var estahVazia = inteiros.Count == 0;
+
+            /* foreach (var inteiro in inteiros)
+             {
+                 Console.WriteLine($"{inteiros.IndexOf(inteiro)} : {inteiro}");
+             }*/
+
+            for (int i = 0; i < inteiros.Count; i++)
+            {
+                Console.WriteLine($"{i} : {inteiros[i]}");
+            }
+            
+
+            
+    }
+        
+    }
+}
