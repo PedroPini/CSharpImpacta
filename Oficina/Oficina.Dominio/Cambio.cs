6376e93f990700699fd0b9695fc77fca2305cf85
PedroPini <augustopini@gmail.com>
2018-11-27 00:13:39 -0200

    Adicionando projeto Oficina

---
+++ Oficina/Oficina.Dominio/Cambio.cs
@@ -0,0 +1,10 @@
+﻿namespace Oficina.Dominio
+{
+    public enum Cambio
+    {
+        Automatico = 1,
+        Manual = 2,
+        CVT = 3,
+        SemiAutomatico = 4 
+    }
+}
---
+++ Oficina/Oficina.Repositorios.SistemaArquivos/CorRepositorio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oficina.Dominio;
+
+namespace Oficina.Repositorios.SistemaArquivos
+{
+    public class CorRepositorio
+    {
+        public List<Cor> Selecionar()
+        {
+            var cores = new List<Cor>();
+
+            foreach(var item in File.ReadAllLines("Dados\\Cor.txt"))
+            {
+                var cor = new Cor();
+                cor.Id = Convert.ToInt32(item.Substring(0, 5));
+                cor.Nome = item.Substring(5);//comeca do parametro 5 que é onde comeca o nome do valor cor EX: 00001|Preto
+                cores.Add(cor);
+            }
+            return cores;
+        }
+    }
+}
